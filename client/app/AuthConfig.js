7a902ad3e14ba72cd951eed2a7ba8268e3851b56
Nikola Cop <nikolacop05@gmail.com>
2021-03-05 10:03:57 -0700

    auth

--- client/app/AuthConfig.js
+++ client/app/AuthConfig.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-yn3n6vll.us.auth0.com'
+export const audience = 'https://squadup.com'
+export const clientId = 'KaR2e8KRT1mN1MmsbkCXw6Nh26dDsDLs'
